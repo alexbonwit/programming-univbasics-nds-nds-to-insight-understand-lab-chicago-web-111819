7287056d7bcb3fe84a30d056090e0ac5b565c4d0
Alex Bonwit <alexbonwit@mac.com>
2019-11-09 01:32:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,9 +9,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
-  while row_index < directors_database.length do
-    puts 
-  end
+  # row_index = 0
+  # while row_index < directors_database.length do
+  #   puts 
+  # end
   
 end

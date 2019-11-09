c69feddfd0814ac4bbb63429b23d33f6532544fe
Alex Bonwit <alexbonwit@mac.com>
2019-11-09 01:30:08 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,13 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  require 'pp'
+  pp directors_database
 end
 
 def print_first_directors_movie_titles
+  row_index = 0
+  while row_index < directors_database.length do
+    puts 
+  
 end
